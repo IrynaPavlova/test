178b171e9b2883562ee5622dba831487b1310579
Pavlova <irochkapavlova85@gmail.com>
2019-12-18 12:02:52 +0200

    Updates

---
+++ precache-manifest.9ccfb8e8454652b58492bd0ed50f5a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489a24e4597a1992d5e9281039d34d7b",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/index.html"
+  },
+  {
+    "revision": "c9d272d46ad905c82869",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/static/css/main.2641aef9.chunk.css"
+  },
+  {
+    "revision": "73af6aeacf5b2c604ec7",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/static/js/2.b9632138.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/static/js/2.b9632138.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9d272d46ad905c82869",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/static/js/main.60541ff5.chunk.js"
+  },
+  {
+    "revision": "8d1c86c8471cc2eab3d2",
+    "url": "/IrynaPavlova/goit-react-hw-01-components.git/static/js/runtime-main.47910206.js"
+  }
+]);
