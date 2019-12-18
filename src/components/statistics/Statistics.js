e9db6559d095eddf47d4a52aa05995d6fb6b29ee
Pavlova <irochkapavlova85@gmail.com>
2019-12-18 11:43:13 +0200

    test

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './statistics.module.css'
+import styles from './statistics.module.css';
 
-const Statistics = ({title, stats}) => (
-
-<section className={`${styles.statistics} ${styles.container}`}>
-  <h2 className={styles.title}>{title}</h2>
-  <ul className={styles.statList}>
-    {stats.map(elem=> (
-    <li key= {elem.id} className={styles.item}>
-      <span className="label">{elem.label}</span>
-      <span className="percentage">{elem.percentage}%</span>
-    </li>))}
-  </ul>
-</section>
-)
-
-Statistics.propTypes = {
-title: PropTypes.string,
-stats: PropTypes.arrayOf(
-  PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
-  })
-)
+function randomColor() {
+  return Math.floor(Math.random() * 256);
 }
 
-export default Statistics
+const Statistics = ({ title, stats }) => (
+  <section className={`${styles.statistics} ${styles.container}`}>
+    <h2 className={styles.title}>{title}</h2>
+    <ul className={styles.statList}>
+      {stats.map(elem => (
+        <li
+          key={elem.id}
+          className={styles.item}
+          style={{
+            backgroundColor: `rgb(${randomColor()}, ${randomColor()}, ${randomColor()}`,
+          }}
+        >
+          <span className="label">{elem.label}</span>
+          <span className="percentage">{elem.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+export default Statistics;
